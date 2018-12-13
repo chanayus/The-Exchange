5863d1a6e0202e98c587189d3a40326f6c810c28
Chayapat Panrod <61070036@kmitl.ac.th>
2018-12-13 16:32:05 +0700

    Upload 2001

---
+++ Exchange rates graph/2001Exchangerates.py
@@ -0,0 +1,16 @@
+""" Exchange """
+import matplotlib.pyplot as plt
+def main():
+    """ function """
+    x = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",\
+     "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+    y = [42.5, 43.1, 45.025, 45.65, 45.375, 45.3, 45.72, 44.05, 44.45, 44.71, 43.935, 44.235]#input rate exchange
+    avg = sum(y)/12
+    plt.plot(x, y, color="blue", marker="o", label="Exchange Rates")
+    plt.axhline(avg, color="red", linestyle="--", label="Average")
+    plt.title("Exchange Rates", color="black", fontsize=18)
+    plt.ylabel("Rate")
+    plt.xlabel("Month")
+    plt.legend()
+    plt.show()
+main()
