d12daeff6ff530ff93c3c2c8f303415990768e2a
Supachai Chanto <42431305+Supachaijunto@users.noreply.github.com>
2018-12-14 07:58:41 +0700

    Update 2007Exchange.py

--- Exchange rates graph/2007Exchange.py
+++ Exchange rates graph/2007Exchange.py
@@ -6,11 +6,11 @@ def main():
      "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
     y = [34.75, 34.025, 32.325, 32.85, 32.9, 31.7, 29.95, 32.535, 31.9, 31.675, 30.7, 30.05]#input rate exchange
     avg = sum(y)/12
-    plt.plot(x, y, color="purple", marker="o", label="Exchange Rate")
+    plt.plot(x, y, color="purple", marker="o", label="Exchange Rates")
     plt.axhline(avg, linestyle="--", label="Average")
     plt.title("Exchange Rate 2007", color="blue", fontsize=18)
-    plt.ylabel("Rate")
-    plt.xlabel("Month")
+    plt.ylabel("Rates")
+    plt.xlabel("Months")
     plt.legend()
     plt.show()
 main()
